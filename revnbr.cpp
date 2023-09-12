186e860987e1f29f02a4d667eaf23173a3b1ff10
Eakond1 <143996703+Eakond1@users.noreply.github.com>
2023-09-12 14:46:42 +0300

    Add files via upload

--- revnbr.cpp
+++ revnbr.cpp
@@ -2,16 +2,16 @@
 #include "easy.h"
 using namespace std;
 int itc_revnbr(int num) {
-     if (num < 100 || num > 999) {
-         return -1;
-     }
-         int reversedNum = 0;
-         while (num > 0) {
-          int digit = num % 10;
-         reversedNum = reversedNum * 10 + digit;
-         num /= 10;
-         }
-          return reversedNum;
-         }
+    int reversed = 0;
+    int remainder;
+    while (num > 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    return reversed;
+}
+
 
 
