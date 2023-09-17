0cddf41aa65e684ced8f0186db9ee8b9beaa250c
Eakond1 <143996703+Eakond1@users.noreply.github.com>
2023-09-17 19:27:13 +0300

    Add files via upload

--- skv.cpp
+++ skv.cpp
@@ -20,13 +20,15 @@ S=a*b;
 return S;
 }
 int itc_str(int a, int b, int c){
-if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a ) {
     return -1;
 }
-int p;
-p= (a+b+c)/2;
-int S;
+double p;
+double pr=a+b+c;
+p=pr/2;
+double S;
 S= sqrt(p*(p-a)*(p-b)*(p-c));
+
 return S;
 }
 double itc_scir(int radius){
